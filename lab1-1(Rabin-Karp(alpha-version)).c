582df0b621534f7186607f578b5171e20b95be58
Nikita Kosarev <51489085+CaptainMorgan4392@users.noreply.github.com>
2019-11-27 20:57:15 +0700

    Update lab1-1(Rabin-Karp(alpha-version)).c

--- lab1-1(Rabin-Karp(alpha-version)).c
+++ lab1-1(Rabin-Karp(alpha-version)).c
@@ -1,156 +1,147 @@
 #define MAX_DEGREE 16
-#define SIZE_OF_BUFFER 4096
+#define SIZE_OF_RING_BUFFER 4096
 
 #include <stdio.h>
 #include <mm_malloc.h>
 #include <memory.h>
-#include <string.h>
 #include <stdbool.h>
 
-unsigned int arrayOfDegrees[16];
+typedef struct _template Template;
+typedef struct _ring_buffer RingBuffer;
 
-typedef struct {
+struct _template {
     unsigned char pattern[MAX_DEGREE + 1];
-    size_t size;
-    unsigned int hash;
-} Template;
+    size_t length;
+    size_t hash;
+    bool allocated;
+};
 
-typedef struct {
-    unsigned char pattern[SIZE_OF_BUFFER + 1];
-    size_t size;
-    unsigned int hash;
-} Buffer;
+struct _ring_buffer {
+    unsigned char pattern[SIZE_OF_RING_BUFFER + 1];
+    size_t length;
+    size_t hash;
+    bool allocated;
+};
 
-typedef struct {
-    Template* t;
-    Buffer* b;
-} Context;
-
-void (*currentAction)(Context* ctx);
-
-void freeDynamicMemory(Context* ctx) {
-    free(ctx -> t);
-    free(ctx -> b);
-    free(ctx);
-}
+unsigned int arrayOfDegrees[MAX_DEGREE];
 
 void calculateDegrees() {
-    for (unsigned int i = 0; i < MAX_DEGREE; i++) {
+    for (size_t i = 0; i < MAX_DEGREE; i++) {
         arrayOfDegrees[i] = (i == 0 ? 1 : arrayOfDegrees[i - 1] * 3);
     }
 }
 
-unsigned int calculateHash(const unsigned char* pattern) {
-    unsigned int answer = 0;
+void getHashFromHash(unsigned char prev, unsigned char next, Template* t, RingBuffer* b) {
+    size_t firstTerm = (b -> hash - (prev % 3)) / 3,
+           secondTerm = (next % 3) * arrayOfDegrees[t -> length - 1];
+    b -> hash = firstTerm + secondTerm;
+}
 
-    for (size_t i = 0; i < MAX_DEGREE; i++) {
-        if (pattern[i] == '\0') {
-            return answer;
-        }
-        answer += (pattern[i] % 3) * arrayOfDegrees[i];
+size_t calculateHash(const unsigned char* pattern, size_t length) {
+    size_t answer = 0;
+    for (size_t i = 0; i < length; i++) {
+        size_t firstPart = ((size_t)pattern[i] % 3),
+               secondPart = arrayOfDegrees[i];
+        answer += firstPart * secondPart;
     }
 
     return answer;
 }
 
-unsigned int getHashFromHash(Context* ctx, const unsigned char* currentBlock, unsigned char prev) {
-    unsigned int currentHash = ctx -> b -> hash;
-
-    return (currentHash - (prev % 3)) / 3 + (currentBlock[ctx -> t -> size - 1] % 3) * arrayOfDegrees[ctx -> t -> size - 1];
-}
-
-void initialiseTemplate(Context* ctx) {
-    if (scanf("%16[^\n]s", ctx -> t -> pattern) == 0) {
+void initialiseTemplate(Template* t) {
+    t -> allocated = false;
+    if (scanf("%16[^\n]s", t -> pattern) == 0) {
         printf("bad input");
-        freeDynamicMemory(ctx);
-        return;
-    } else {
-        unsigned char useless = (unsigned char)getchar();
-        if (useless != '\n') {
-            printf("bad input");
-            freeDynamicMemory(ctx);
-            return;
-        }
-    }
-
-    ctx -> t -> size = strlen((char*)ctx -> t -> pattern);
-    ctx -> t -> hash = calculateHash(ctx -> t -> pattern);
-    printf("%d ", ctx -> t -> hash);
-}
-
-void initialiseBuffer(Context* ctx) {
-    if (scanf("%4096[^\xff]s", ctx -> b -> pattern) == 0) {
-        freeDynamicMemory(ctx);
+        free(t);
         return;
     }
+    t -> allocated = true;
+    t -> length = strlen((char*)t -> pattern);
+    t -> hash = calculateHash(t -> pattern, t -> length);
+    printf("%zu ", t -> hash);
 
-    ctx -> b -> size = strlen((char*)ctx -> b -> pattern);
+    if (getchar() != '\n') {
+        printf("bad input");
+        t -> allocated = false;
+    }
 }
 
-void initialiseContext(Context* ctx) {
-    ctx -> t = calloc(1, sizeof(Template));
-    currentAction = initialiseTemplate;
-    currentAction(ctx);
-    ctx -> b = calloc(1, sizeof(Buffer));
-    currentAction = initialiseBuffer;
-    currentAction(ctx);
+void initialiseBuffer(RingBuffer* b) {
+    b -> allocated = false;
+    if (scanf("%4096[^\xff]s", b -> pattern) == 0) {
+        printf("bad input");
+        return;
+    }
+    b -> allocated = true;
+    b -> length = strlen((char*)b -> pattern);
 }
 
-bool areHashesEqual(Context* ctx) {
-    return ctx -> t -> hash == ctx -> b -> hash;
-}
-
-void compareLines(Template* t, const unsigned char* toCompare, size_t currentIndex) {
-    for (size_t i = 0; i < t -> size; i++) {
+void compareLines(const unsigned char* first, const unsigned char* second, size_t currentIndex) {
+    for (size_t i = 0; i < strlen((char*)first); i++) {
         printf("%zu ", currentIndex + i);
-        if (t -> pattern[i] != toCompare[i]) {
+        if (first[i] != second[i])
             return;
-        }
     }
 }
 
-void startAlgorithm(Context* ctx) {
-    static size_t currentIndex = 1;
-    unsigned char* blockWithEqualLength = (unsigned char*)calloc(MAX_DEGREE + 1, sizeof(unsigned char));
-    size_t currentPositionInBuffer = 0;
+void RabinKarpSearch(Template* t, RingBuffer* b) {
+    size_t currentIndex = 1;
+    bool isBeginOfBuffer = true;
+    unsigned char prev = 0;
+    unsigned char* blockWithEqualLength = (unsigned char*)calloc(t -> length + 1, sizeof(unsigned char));
+    while (true) {
+        for (size_t i = 0; i < b -> length - t -> length + 1; i++) {
+            if (i == 0) {
+                memcpy(blockWithEqualLength, b -> pattern, t -> length);
+            } else {
+                prev = blockWithEqualLength[0];
+                for (size_t j = 1; j < t -> length; j++) {
+                    blockWithEqualLength[j - 1] = blockWithEqualLength[j];
+                }
+                blockWithEqualLength[t -> length - 1] = b -> pattern[t -> length + i - 1];
+                currentIndex++;
+            }
 
-    for (size_t i = 0; i < ctx -> b -> size - ctx -> t -> size + 1; i++) {
-        unsigned char prevSymbol = 0;
-        if (i == 0) {
-            memcpy(blockWithEqualLength, ctx -> b -> pattern, ctx -> t -> size);
-            currentPositionInBuffer = strlen((char*)blockWithEqualLength);
+            if (isBeginOfBuffer) {
+                b -> hash = calculateHash(blockWithEqualLength, t -> length);
+                isBeginOfBuffer = false;
+            } else {
+                getHashFromHash(prev, blockWithEqualLength[t -> length - 1], t, b);
+            }
+            if (t -> hash == b -> hash) {
+                compareLines(t -> pattern, blockWithEqualLength, currentIndex);
+            }
+        }
+
+        if (!feof(stdin) && b -> length == SIZE_OF_RING_BUFFER) {
+            isBeginOfBuffer = true;
+            initialiseBuffer(b);
+            currentIndex += t -> length;
         } else {
-            prevSymbol = blockWithEqualLength[0];
-            memcpy(blockWithEqualLength, blockWithEqualLength + 1, ctx -> t -> size - 1);
-            blockWithEqualLength[ctx -> t -> size - 1] = ctx -> b -> pattern[currentPositionInBuffer - 1];
+            break;
         }
-
-        if (currentPositionInBuffer == strlen((char*)blockWithEqualLength)) {
-            ctx -> b -> hash = calculateHash(blockWithEqualLength);
-        } else {
-            ctx -> b -> hash = getHashFromHash(ctx, blockWithEqualLength, prevSymbol);
-        }
-
-        if (areHashesEqual(ctx)) {
-            compareLines(ctx -> t, blockWithEqualLength, currentIndex);
-        }
-        currentIndex++;
-        currentPositionInBuffer++;
     }
 
-    if (!feof(stdin)) {
-        initialiseBuffer(ctx);
-        currentIndex += ctx -> t -> size - 1;
-        startAlgorithm(ctx);
-    }
+    free(blockWithEqualLength);
 }
 
 int main() {
     calculateDegrees();
-    Context* context = calloc(1, sizeof(Context));
-    currentAction = initialiseContext;
-    currentAction(context);
-    currentAction = startAlgorithm;
-    currentAction(context);
+    Template* template = calloc(1, sizeof(Template));
+    initialiseTemplate(template);
+    if (!template -> allocated) {
+        free(template);
+        return 0;
+    }
+    RingBuffer* buffer = calloc(1, sizeof(RingBuffer));
+    initialiseBuffer(buffer);
+    if (!buffer -> allocated) {
+        free(template);
+        free(buffer);
+        return 0;
+    }
+    RabinKarpSearch(template, buffer);
+    free(template);
+    free(buffer);
     return 0;
 }
